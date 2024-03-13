13ee9122e3dd5f5f49ee4f73cb87911a5a43f007
Mauricio Mosquera <mauriciomos@hotmail.com>
2024-03-13 11:51:26 -0300

    Dependencias, header y footer

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,35 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { MatCardModule } from '@angular/material/card';
+import { MatAutocompleteModule } from '@angular/material/autocomplete';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatInputModule } from '@angular/material/input';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import { MatSelectModule } from '@angular/material/select';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [
+    RouterOutlet,
+    MatCardModule,
+    MatAutocompleteModule,
+    MatButtonModule,
+    MatIconModule,
+    MatPaginatorModule,
+    MatDialogModule,
+    MatInputModule,
+    MatSlideToggleModule,
+    MatSelectModule
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'costos-mumo';
+
+
 }
